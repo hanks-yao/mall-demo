d9d25c51528661aa4755bebcce31bf035edd15b2
yuanyao7 <yaoyuan93@qq.com>
2022-01-26 15:19:03 +0800

    feat: 升级view-design版本

--- vue.config.js
+++ vue.config.js
@@ -26,14 +26,14 @@ let externals = {
 
 // 使用CDN的内容
 let cdn = {
-  css: ["https://cdn.jsdelivr.net/npm/view-design@4.1.1/dist/styles/iview.css"],
+  css: ["https://cdn.jsdelivr.net/npm/view-design@4.7.0/dist/styles/iview.css"],
   js: [
     // vue must at first!
     "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
     "https://cdn.jsdelivr.net/npm/vuex@3.1.2/dist/vuex.min.js",
     "https://cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js",
     "https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
-    "https://cdn.jsdelivr.net/npm/view-design@4.1.1/dist/iview.min.js",
+    "https://cdn.jsdelivr.net/npm/view-design@4.7.0/dist/iview.min.js",
     "https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js"
   ]
 };
